e1f7804d90a07bc1b275c05385c8d1838ff981cd
Jasper Vercammen <jasper.vercammen@icapps.com>
2018-05-02 14:33:25 +0200

    Remove lottie

--- screens/SavedNameDetailScreen.js
+++ screens/SavedNameDetailScreen.js
@@ -7,10 +7,6 @@ let { Lottie } = DangerZone
 const { width } = Dimensions.get('window')
 
 class SavedNameDetailScreen extends Component {
-  componentDidMount () {
-    this.animation.reset()
-    this.animation.play()
-  }
   render () {
     const params = this.props.navigation.state.params || {}
 
@@ -18,19 +14,6 @@ class SavedNameDetailScreen extends Component {
       <View style={styles.container}>
         <View style={styles.topContainer}>
           <Image source={params.baby.gender === 'male' ? require('@assets/boy.png') : require('@assets/girl.png')} style={styles.image} />
-        </View>
-        <View style={styles.animation}>
-          <Lottie
-            ref={animation => {
-              this.animation = animation
-            }}
-            style={{
-              width,
-              height: 200,
-              backgroundColor: 'transparent'
-            }}
-            source={require('@assets/sheep.json')}
-          />
         </View>
         <Text style={styles.name}>{params.baby.name}</Text>
         <View style={styles.bottomContainer}>
