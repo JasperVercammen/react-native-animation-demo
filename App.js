ff8cdcc985be94f54b70c0c85ba33417d5a3e8be
Jasper Vercammen <jasper.vercammen@icapps.com>
2018-05-02 08:21:22 +0200

    Scale demo

--- App.js
+++ App.js
@@ -1,10 +1,186 @@
 import React from 'react'
-import AppNavigator from '@screens/AppNavigator'
+import { Button, Dimensions, Easing, View, Animated, ImageBackground, StyleSheet } from 'react-native'
+import { StackNavigator } from 'react-navigation'
+import Navigator1 from '@screens/Navigator1'
+import Navigator2 from '@screens/Navigator2'
 
-export default class App extends React.Component {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    position: 'relative'
+  },
+  navigatorWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'nowrap'
+  },
+  title: {
+    backgroundColor: 'transparent',
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 16,
+    position: 'absolute',
+    top: 65,
+    width: '100%',
+    textAlign: 'center'
+  }
+})
+
+class MyHomeScreen extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      inSwitchMode: false,
+      banking: true,
+      scaleBanking: new Animated.Value(1),
+      scaleInvesting: new Animated.Value(1),
+      translateXBanking: new Animated.Value(0),
+      translateXInvesting: new Animated.Value(0),
+      opacityTitle: new Animated.Value(0),
+      translateYTitle: new Animated.Value(-40)
+    }
+  }
+
+  play = () => {
+    if (this.state.inSwitchMode) {
+      this.gotoFullMode()
+    } else {
+      this.gotoSwitchMode()
+    }
+  };
+
+  gotoSwitchMode = () => {
+    this.setState({ inSwitchMode: true })
+    Animated.parallel([
+      Animated.timing(this.state.scaleBanking, {
+        toValue: 0.6,
+        easing: Easing.easeOut,
+        duration: 350,
+        useNativeDriver: true
+      }),
+      Animated.timing(this.state.translateXBanking, {
+        toValue: -Dimensions.get('window').width / 2,
+        easing: Easing.easeOut,
+        duration: 350,
+        useNativeDriver: true
+      }),
+      Animated.timing(this.state.scaleInvesting, {
+        toValue: 0.6,
+        easing: Easing.easeOut,
+        duration: 350,
+        useNativeDriver: true
+      }),
+      Animated.timing(this.state.translateXInvesting, {
+        toValue: -Dimensions.get('window').width - 30,
+        easing: Easing.easeOut,
+        duration: 350,
+        useNativeDriver: true
+      }),
+      Animated.timing(this.state.opacityTitle, {
+        toValue: 1,
+        easing: Easing.easeOut,
+        duration: 350,
+        useNativeDriver: true
+      }),
+      Animated.timing(this.state.translateYTitle, {
+        toValue: 0,
+        easing: Easing.easeOut,
+        duration: 350,
+        useNativeDriver: true
+      })
+    ]).start(_ => {})
+  };
+
+  gotoFullMode = () => {
+    Animated.parallel([
+      Animated.timing(this.state.scaleBanking, {
+        toValue: 1,
+        easing: Easing.easeOut,
+        duration: 350,
+        useNativeDriver: true
+      }),
+      Animated.timing(this.state.translateXBanking, {
+        toValue: this.state.banking ? -Dimensions.get('window').width : 0,
+        easing: Easing.easeOut,
+        duration: 350,
+        useNativeDriver: true
+      }),
+      Animated.timing(this.state.scaleInvesting, {
+        toValue: 1,
+        easing: Easing.easeOut,
+        duration: 350,
+        useNativeDriver: true
+      }),
+      Animated.timing(this.state.translateXInvesting, {
+        toValue: this.state.banking ? -Dimensions.get('window').width : 0,
+        easing: Easing.easeOut,
+        duration: 350,
+        useNativeDriver: true
+      }),
+      Animated.timing(this.state.opacityTitle, {
+        toValue: 0,
+        easing: Easing.easeOut,
+        duration: 350,
+        useNativeDriver: true
+      }),
+      Animated.timing(this.state.translateYTitle, {
+        toValue: -40,
+        easing: Easing.easeOut,
+        duration: 350,
+        useNativeDriver: true
+      })
+    ]).start(_ => {
+      this.setState({ inSwitchMode: false, banking: !this.state.banking })
+    })
+  };
+
   render () {
     return (
-      <AppNavigator />
+      <ImageBackground style={styles.container} resizeMode='stretch' source={require('@assets/rectangle12.png')}>
+        {/* <Button onPress={this.play} title='toggle View' /> */}
+        {this.state.inSwitchMode && (
+          <Animated.Text
+            style={[
+              styles.title,
+              {
+                opacity: this.state.opacityTitle,
+                transform: [ { translateY: this.state.translateYTitle } ]
+              }
+            ]}>
+            Select your environment
+          </Animated.Text>
+        )}
+        <View style={styles.navigatorWrapper}>
+          <Animated.View
+            style={{
+              width: Dimensions.get('window').width,
+              transform: [ { scale: this.state.scaleBanking }, { translateX: this.state.translateXBanking } ]
+            }}>
+            <Navigator1 />
+          </Animated.View>
+          <Animated.View
+            style={{
+              width: Dimensions.get('window').width,
+              transform: [ { scale: this.state.scaleInvesting }, { translateX: this.state.translateXInvesting } ]
+            }}>
+            <Navigator2 />
+          </Animated.View>
+        </View>
+      </ImageBackground>
     )
   }
 }
+
+MyHomeScreen.navigationOptions = {
+  title: 'Welcome',
+  header: null
+}
+
+const SimpleStack = StackNavigator({
+  Home: {
+    screen: MyHomeScreen
+  }
+})
+
+export default SimpleStack
--- screens/AppNavigator.js
+++ screens/AppNavigator.js
@@ -1,62 +1,7 @@
 import React from 'react'
 import { StackNavigator } from 'react-navigation'
-import { Animated, Easing } from 'react-native'
 import HomeScreen from '@screens/HomeScreen'
 import HeaderButton from '@components/HeaderButton'
-
-const transitionConfig = (props) => {
-  return {
-    transitionSpec: {
-      duration: 1750,
-      easing: Easing.bezier(0, 0.12, 0.88, 1),
-      timing: Animated.timing,
-      useNativeDriver: true
-    },
-    containerStyle: {
-      backgroundColor: 'transparent'
-    },
-    screenInterpolator: sceneProps => {
-      const { position, progress, layout, scene, index } = sceneProps
-      const toIndex = index
-      const thisSceneIndex = scene.index
-      const width = layout.initWidth
-
-      if (toIndex !== thisSceneIndex && thisSceneIndex !== toIndex - 1 && thisSceneIndex !== toIndex + 1) return { opacity: 0 }
-
-      const translateX = position.interpolate({
-        inputRange: [thisSceneIndex - 1, thisSceneIndex - 0.8, thisSceneIndex - 0.2, thisSceneIndex, thisSceneIndex + 0.2, thisSceneIndex + 0.8, thisSceneIndex + 1],
-        outputRange: [width, width, 0, 0, 0, -width, -width]
-      })
-
-      const rotate = position.interpolate({
-        inputRange: [thisSceneIndex - 1, thisSceneIndex - 0.8, thisSceneIndex - 0.2, thisSceneIndex, thisSceneIndex + 0.2, thisSceneIndex + 0.8, thisSceneIndex + 1],
-        outputRange: [ '-9deg', '-9deg', '0deg', '0deg', '0deg', '9deg', '9deg' ]
-      })
-
-      const scale = progress.interpolate({
-        inputRange: [0, 0.1, 0.5, 0.9, 1],
-        outputRange: [index !== thisSceneIndex ? 1 : 0.6, 0.6, 0.6, 0.6, index !== thisSceneIndex ? 0.6 : 1]
-      })
-
-      let translateY = 0
-      if (index !== thisSceneIndex) {
-        translateY = progress.interpolate({
-          inputRange: [0, 0.2, 0.8, 1],
-          outputRange: [0, 0, -30, -30]
-        })
-      }
-
-      if (index === thisSceneIndex) {
-        translateY = progress.interpolate({
-          inputRange: [0, 0.2, 0.8, 1],
-          outputRange: [-30, -30, 0, 0]
-        })
-      }
-
-      return { transform: [{ translateX }, { translateY }, {scale}, {rotate}] }
-    }
-  }
-}
 
 const AppNavigator = StackNavigator({
   Home: {
@@ -72,8 +17,7 @@ const AppNavigator = StackNavigator({
     count: 0
   },
   initialRouteName: 'Home',
-  headerMode: 'float',
-  transitionConfig
+  headerMode: 'float'
 })
 
 export default AppNavigator
