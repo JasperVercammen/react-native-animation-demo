dc2162e19d21c5018d7016e31e681f23e56c0132
Jasper Vercammen <jasper.vercammen@icapps.com>
2018-05-02 14:10:12 +0200

    More fix

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import {View} from 'react-native'
 import AppNavigator from '@screens/AppNavigator'
 
 export default class App extends React.Component {
   render () {
     return (
+      // <View style={{flex: 1, transform: [{scale: 0.3}]}}>
       <AppNavigator />
+      // </View>
     )
   }
 }
